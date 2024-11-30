a73e219ce6cb17f3a7d64feda30226bc6f0182ad
113257002-Peng <113257002@g.nccu.edu.tw>
2024-11-30 22:37:40 +0800

    Initial commit

---
+++ frontend/src/config/site.ts
@@ -0,0 +1,65 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "FooDiary",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Login",
+      href: "/login",
+    },
+    {
+      label: "Signup",
+      href: "/signup",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Info Form",
+      href: "/info-form",
+    },
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    {
+      label: "Login",
+      href: "/login",
+    },
+    {
+      label: "Signup",
+      href: "/signup",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Info Form",
+      href: "/info-form",
+    },
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+  ],
+  links: {
+    github: "https://github.com/nextui-org/nextui",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui-docs-v2.vercel.app",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
